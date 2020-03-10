f575fba87f76f2e0aada7d9bc766ea51c17185c1
Fadoua.l <Fadoua.l@DESKTOP-0S2D563>
2020-03-10 15:39:14 +0000

    FADOUA TEST SOUK

--- Scripts/testing ne/Script1583853199747.groovy
+++ Scripts/testing ne/Script1583853199747.groovy
@@ -22,6 +22,8 @@ WebUI.navigateToUrl('https://preprod.iadfrance.fr/')
 
 WebUI.click(findTestObject('Object Repository/ne testing/Page_iad - Vente achat location maison appa_3091d3/a_Estimer'))
 
+WebUI.waitForElementPresent(findTestObject('ne testing/Page_iad - Vente achat location maison appa_3091d3/a_Vendre'), 0)
+
 WebUI.click(findTestObject('Object Repository/ne testing/Page_iad - Vente achat location maison appa_3091d3/a_Vendre'))
 
 WebUI.verifyElementNotClickable(findTestObject('ne testing/Page_iad - Vente achat location maison appa_3091d3/a_Vendre'))
