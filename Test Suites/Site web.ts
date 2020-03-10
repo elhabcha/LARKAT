3ad0c491bdbfb11c24de41c3ed93062f04ae0021
rajaa.l <rajaa.l@DESKTOP-M52VN0N>
2020-03-10 14:26:55 +0000

    Test site public

---
+++ Test Suites/Site web.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Site web</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d89147b1-e5fa-4253-84b7-efe89448eed3</testSuiteGuid>
+</TestSuiteEntity>
